5f8ff812b1a30c547f77c1816ca4d6551b45aa88
CrazyBunnyz <52201618+CrazyBunnyz@users.noreply.github.com>
2019-06-25 18:00:40 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,8 @@
+navigator.serviceWorker.getRegistrations().then(function(registrations) {
+ for(let registration of registrations) {
+  registration.unregister()
+} })
+
 self.addEventListener('install', function(event) {
   //console.log("install sw done");
   event.waitUntil(
